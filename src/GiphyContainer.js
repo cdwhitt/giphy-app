aa6404b2c93bc093672fb2927f997c1c4c3c766c
Casey Whittaker <cw@roofrally.com>
2020-08-13 13:05:56 -0400

    Create initial app

---
+++ src/GiphyContainer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+export const GiphyContainer = (props) => {
+  return <Header>I am the Giphy Container Element</Header>;
+};
